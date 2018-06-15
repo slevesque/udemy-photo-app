fae1903aacd4760c6a90c13e59ee15c008b4aaca
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-15 11:58:36 -0400

    Add credit card form inputs to  the registration form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end

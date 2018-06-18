6b2efa23fb365c0cccb95ebd1c6a136f4c76d139
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-18 15:42:41 -0400

    Fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,8 +4,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   process resize_to_limit: [300, 300]
 
